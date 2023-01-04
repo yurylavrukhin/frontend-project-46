afbd294241dbda8c845beb69615d1c9c9bb64ab5
Yury Lavrukhin <me@yurylavrukhin.com>
2023-01-04 14:27:05 +0700

    feat(help): format option, arguments, custom help message

--- gendiff.js
+++ gendiff.js
@@ -2,7 +2,10 @@
 import { program } from 'commander';
 
 program
-.version('0.0.0')
-.description(`Compares two configuration files and shows a difference.`);
+  .version('0.0.0')
+  .description(`Compares two configuration files and shows a difference.`)
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .helpOption('-h, --help', 'output usage information');
 
 program.parse(process.argv);
