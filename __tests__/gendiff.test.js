00c13896e73243c441d6d0bd4f9260892eaeecc9
Yury Lavrukhin <me@yurylavrukhin.com>
2023-01-20 14:56:23 +0700

    refactor: move fixtures to the root

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,57 +4,45 @@ import path from 'path';
 import fs from 'fs';
 import genDiff from '../gendiff.js';
 
-/* eslint-disable */
+// eslint-disable-next-line no-underscore-dangle
 const __filename = fileURLToPath(import.meta.url);
+// eslint-disable-next-line no-underscore-dangle
 const __dirname = path.dirname(__filename);
 
-const getFixturePath = (filename) =>
-  path.join(__dirname, '..', '__tests__', '__fixtures__', filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('yaml. should handle: untouched, touched, deleted, added fields', () => {
-  const firstPath = getFixturePath('./file1.yml');
-  const secondPath = getFixturePath('./file2.yml');
-
-  const expectedPath = getFixturePath('./expected.txt');
-
-  const actualOutput = genDiff(firstPath, secondPath);
-  const expectedOutput = fs.readFileSync(expectedPath).toString().trim();
-
-  expect(actualOutput).toBe(expectedOutput);
-});
-
-test('stylish. should handle: untouched, touched, deleted, added fields', () => {
+test('stylish format: should generate the diff', () => {
   const firstPath = getFixturePath('./file1.yml');
   const secondPath = getFixturePath('./file2.json');
 
-  const expectedPath = getFixturePath('./expected.txt');
+  const expectedPath = getFixturePath('./expected-stylish.txt');
 
   const actualOutput = genDiff(firstPath, secondPath);
-  const expectedOutput = fs.readFileSync(expectedPath).toString().trim();
+  const expectedStylishOutput = fs.readFileSync(expectedPath).toString();
 
-  expect(actualOutput).toBe(expectedOutput);
+  expect(actualOutput).toBe(expectedStylishOutput);
 });
 
-test('plain. should handle: untouched, touched, deleted, added fields', () => {
+test('plain format: should generate the diff', () => {
   const firstPath = getFixturePath('./file1.yml');
   const secondPath = getFixturePath('./file2.json');
 
   const expectedPath = getFixturePath('./expected-plain.txt');
 
   const actualOutput = genDiff(firstPath, secondPath, 'plain');
-  const expectedOutput = fs.readFileSync(expectedPath).toString().trim();
+  const expectedPlainOutput = fs.readFileSync(expectedPath).toString();
 
-  expect(actualOutput).toBe(expectedOutput);
+  expect(actualOutput).toBe(expectedPlainOutput);
 });
 
-test('json. should handle: untouched, touched, deleted, added fields', () => {
+test('json format: should generate the diff', () => {
   const firstPath = getFixturePath('./file1.yml');
   const secondPath = getFixturePath('./file2.json');
 
   const expectedPath = getFixturePath('./expected-json.txt');
 
   const actualOutput = genDiff(firstPath, secondPath, 'json');
-  const expectedOutput = fs.readFileSync(expectedPath).toString().trim();
+  const expectedJsonOutput = fs.readFileSync(expectedPath).toString();
 
-  expect(actualOutput).toBe(expectedOutput);
+  expect(actualOutput).toBe(expectedJsonOutput);
 });
